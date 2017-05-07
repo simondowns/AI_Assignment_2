a4d45fa3d71b8a1d8208a85d53bd370631e1f136
aqualupus <steven@engineeringtaylored.com>
2017-05-07 14:05:20 +1000

    Mostly Working TT

--- AI_Assignment_2/Program.cs
+++ AI_Assignment_2/Program.cs
@@ -10,7 +10,7 @@ namespace AI_Assignment_2
 
 			//this obvioulsy needs to be better
 			BuildKB KB = new BuildKB("test1.txt");
-			TruthTable TT = new TruthTable(KB.Implies, KB.Vars,KB.TrueVars, "h");
+			TruthTable TT = new TruthTable(KB.Implies, KB.Vars,KB.TrueVars, "d");
 			Console.WriteLine(TT.BuildTT());
 			Console.ReadLine();
 
